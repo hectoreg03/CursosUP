1b360be03f4bd332812f377306e152321f287378
hectoreg03 <96084225+hectoreg03@users.noreply.github.com>
2022-02-27 22:17:56 -0600

    Update ejercicio.py

--- POO2022/ejercicio.py
+++ POO2022/ejercicio.py
@@ -13,6 +13,14 @@ class Trabajador():
     def getsalary(self):
         return self.salario
     
+    def getname(self):
+        return self.nombre
+
+     
+class TrabajadorAntiguo(Trabajador):
+    def getsalary(self):
+        return self.salario+(1000*self.antiguedad)    
+
 
 class Gerente(Trabajador):
     def __init__(self, nm, r):
@@ -24,12 +32,9 @@ class Gerente(Trabajador):
         print("El gerente ",self.nombre," esta gerenciando")
 
     def nuevo_salario(self, empleado : TrabajadorAntiguo, nuevo_salario):
-        if empleado.salario < nuevo_salario :
-            empleado.salario = nuevo_salario - (1000*self.antiguedad)
-        
-class TrabajadorAntiguo(Trabajador):
-    def getsalary(self):
-        return self.salary+(1000*self.antiguedad)
+        if empleado.salario+(1000*empleado.antiguedad) < nuevo_salario :
+            empleado.salario = nuevo_salario - (1000*empleado.antiguedad)
+
 
 class Programador(TrabajadorAntiguo):
     def __init__(self, nm, r):
@@ -44,12 +49,28 @@ class Programador(TrabajadorAntiguo):
 class Secretaria(TrabajadorAntiguo):
     def __init__(self, nm, r):
         self.nombre = nm
-        self.salario = 5000
+        self.salario = 10000
         self.antiguedad = r
         
     def trabajar(self):
         print("La secretaria ",self.nombre," esta apoyando")
 
+
+rob= Programador("Robert", 10)
+mak= Gerente("Mark", 5)
+loi= Secretaria("Louis", 6)
+rob.trabajar()
+mak.trabajar()
+loi.trabajar()
+print(rob.getname()," ha ganado ",rob.getsalary()," por trabajar")
+print(mak.getname()," ha ganado ",mak.getsalary()," por trabajar")
+print(loi.getname()," ha ganado ",loi.getsalary()," por trabajar")
+mak.nuevo_salario(rob,32000)
+rob.trabajar()
+print(rob.getname()," ha ganado ",rob.getsalary()," por trabajar")
+mak.nuevo_salario(rob,38000)
+rob.trabajar()
+print(rob.getname()," ha ganado ",rob.getsalary()," por trabajar")
 
 def main():
     empresa=[]
@@ -77,6 +98,3 @@ def main():
                 empresa.append(r)
             else:
                 print("Ha dado un puesto inavlido, vuelva a ingresarlo\n")
-    consultas = int(input("Ingrese la cantidad de consultas que desea hacer\n"))
-    for i in range(consultas):
-        print("Hola")
